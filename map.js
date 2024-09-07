c5786e1a634fbfd118a9e960698f411d51119e37
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-09-07 10:13:39 +0530

    updated

--- map.js
+++ map.js
@@ -1,22 +1,31 @@
-const arr=[5,1,3,2,6];
+// const arr=[5,1,3,2,6];
 //double=[10,2,6,4,12];
 //triple=[15,3,9,6,18];
 //binary=["101","1","10","110"];
 
-function double(x){
-    return x*2;
-}
-const output=arr.map(double);
-console.log(output);
+// function double(x){
+//     return x*2;
+// }
+// const output=arr.map(double);
+// console.log(output);
+
+// function triple(x){
+//     return x*3;
+// }
+// const output1=arr.map(triple);
+// console.log(output1);
+
+// function binary(x){
+//     return x.toString(2);
+// }
+// const output2=arr.map(binary);
+// console.log(output2);
+
+
+const arr=[3,5,6,7,8];
 
 function triple(x){
     return x*3;
 }
-const output1=arr.map(triple);
-console.log(output1);
-
-function binary(x){
-    return x.toString(2);
-}
-const output2=arr.map(binary);
-console.log(output2);
+const output=arr.map(triple);
+console.log(output);
