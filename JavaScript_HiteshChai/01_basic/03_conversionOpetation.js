7aa43f6b2aff16260f0985d06a921ca7dcedd99f
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-08 16:49:13 +0530

    conversion completed of number,string and boolean

---
+++ JavaScript_HiteshChai/01_basic/03_conversionOpetation.js
@@ -0,0 +1,29 @@
+// let score="satya kumar"; //string
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueIntNumber=Number(score);
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);     
+
+//33=>33
+//33abc=>NAN
+//true=>1 false=>0
+//null=>0
+//undefined=>0
+
+let isloggened=0; //string
+let booleanIsloggened=Boolean(isloggened);
+// console.log( typeof booleanIsloggened);
+// console.log(booleanIsloggened); //true
+
+/*
+1=>true 0=>false
+""=>false  "satya"=>true
+*/
+
+let somenumber=1234; //bigint
+let stringNumber=String(somenumber);
+console.log(typeof stringNumber); 
+console.log(stringNumber); 
+
