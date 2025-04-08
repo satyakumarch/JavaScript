c4931defbaa3eb1f11ea26faa016480dd20f9723
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-08 16:17:34 +0530

    datatype completed

---
+++ JavaScript_HiteshChai/01_basic/02_variable.js
@@ -0,0 +1,20 @@
+let  id=12203506
+let name = "satyakumar";
+let isloggened=true;
+let user;
+let useremail=null;
+let useraddress=undefined;;
+
+console.table([id,name,isloggened,user,useremail,useraddress]);
+
+//number
+//bigint
+//boolean
+//string    
+//symbol
+//null
+//undefined
+//object
+//array
+//function
+//object
