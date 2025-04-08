4e81ef6af6c543e7eab26a52a470fc0eca5c6dd2
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-08 15:55:10 +0530

    javascript  bbasic coverd like let, var, const , comment , scopt

---
+++ JavaScript_HiteshChai/01_basic/01_vaiable.js
@@ -0,0 +1,9 @@
+const accountId = 1234567890;
+let accountname="satya kumar";
+var accountgmail="satyakumar@gmail.com";
+accountcity="Nepal";
+let accountaddress;
+
+// accountId=34231243;// cannot change the account id because of const 
+console.log(accountId);
+console.table([accountId,accountname,accountgmail,accountcity,accountaddress]);
--- JavaScript_HiteshChai/satya.js
+++
@@ -1,2 +0,0 @@
-hrllo dayi
-a
