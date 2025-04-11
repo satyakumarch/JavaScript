d04b58ccb2910d19371057dbfb4e6bbb9d665453
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-12 04:05:19 +0530

    For loop with array

---
+++ JavaScript_HiteshChai/05_loop/01_forLoop.js
@@ -0,0 +1,28 @@
+//multiplication table
+// const n=20;
+// for(let i=1;i<=10;i++){
+//     console.log(n+" * "+i+" = ",n*i);
+// };
+
+
+// for(i=1;i<10;i++){
+//     const element=i;
+//     if(i==5){
+//         console.log(`${i} is the best`);
+//     }
+//     console.log(element);
+// }
+
+//multiplication table of the all number 1 to  10
+// for(let i=1;i<=10;i++){
+//     console.log(`outer value : ${i}`)
+//     for(let j=1;j<=10;j++){
+//         console.log(i+" * "+j+" = ",i*j);
+//     }
+// }
+
+// let name=["ram","shyam","mohan","gita","rita","sohan"];
+// for(let i=0;i<name.length;i++){
+//     const element=name[i];
+//     console.log(i+" is : "+element);
+// }
