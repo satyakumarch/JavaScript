55418eeb0cb3f528928df2195813b81488fb475a
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-12 02:26:01 +0530

    if else statement

---
+++ JavaScript_HiteshChai/04_controlFlow/01_conditions.js
@@ -0,0 +1,18 @@
+// const score=300;
+// if(score>200){
+//     let power ="fly";
+//     console.log(`you can achieve ${score}`);
+// }
+// console.log(`you can get ${score}`);
+
+
+const age=55;
+if(age<18){
+    console.log("The man is child");
+}else if(age>18 && age<30){
+    console.log("The man is adult");
+}else if(age>30 && age<50){
+    console.log("the man is young");
+}else{
+    console.log("the man is old");
+}
