27550b93def4bdbf6b0431311d53fd778e74bfdf
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-11 16:38:30 +0530

    Arrow function completed

---
+++ JavaScript_HiteshChai/03_basic/02_scope.js
@@ -0,0 +1,9 @@
+ let a=50;
+if(true){
+    let a=10;
+    const b=20;
+    var c=40;
+}
+// console.log(a);//ReferenceError: a is not defined
+ console.log(b);
+// console.log(c);
---
+++ JavaScript_HiteshChai/03_basic/03_arrow.js
@@ -0,0 +1,34 @@
+// function chai(){
+//   let username="satya";
+//   console.log(this.username());  
+// }
+// console.log(chai());
+
+// const chai=function(){
+//     let username="satya",
+//     console.log(this.username);
+// }
+// chai();
+
+// const chai=()=>{
+//     let username="mohan"
+//     console.log(this);
+// }
+// chai();
+
+
+//one method for write a arraw function////////
+// const add=(num1,num2)=>{
+//     return num1+num2;
+
+// }
+// console.log(add(3,4));
+
+
+
+//second method for writing a arrow function///
+// const add=(num1,num2)=> num1+num2;
+// console.log(add(3,4));
+
+const add=(num1,num2)=> ({username:"sayta kumar"});
+console.log(add(3,4));
