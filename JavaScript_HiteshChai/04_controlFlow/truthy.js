de45700b01797665f26806ab2f688e24e91209db
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-12 03:14:39 +0530

    ternary operator and Nullish coaescing operator

---
+++ JavaScript_HiteshChai/04_controlFlow/truthy.js
@@ -0,0 +1,13 @@
+//Nullish Coalescing operator(??): null undefined
+
+let val1;
+// val1=10 ?? 5 ;
+//  val1=null ?? 10;
+val1 = undefined ?? 20;
+// console.log(val1);
+
+//Ternary operator
+// condition ? true :false
+
+const age=10;
+ age>10?console.log("adult"):console.log("youth");
