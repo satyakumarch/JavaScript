1c5c6227e838c4cf9e4a5322155a764f2a05870c
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-12 02:32:21 +0530

    switch statement of month

---
+++ JavaScript_HiteshChai/04_controlFlow/switch.js
@@ -0,0 +1,42 @@
+const month=2;
+switch(month){
+    case 1:
+    console.log("the month is january");
+    break;
+    case 2:
+    console.log("the month is february");
+    break;
+    case 3:
+    console.log("the month is March");
+    break;
+    case 4:
+    console.log("the month is April");
+    break;
+    case 5:
+    console.log("the month is May");
+    break;
+    case 6:
+    console.log("the month is june");
+    break;
+    case 7:
+    console.log("the month is july");
+    break;
+    case 8:
+    console.log("the month is August");
+    break;
+    case 9:
+    console.log("the month is September");
+    break;
+    case 10:
+    console.log("the month is october");
+    break;
+    case 11:
+    console.log("the month is November");
+    break;
+    case 12:
+    console.log("the month is December");
+    break;
+    default:
+    console.log("the month data are invalid");
+
+}
