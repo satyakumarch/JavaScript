e5ba6c4ff0f1dd5b28e78d06d0d9381cc64a9fe3
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-14 19:54:44 +0530

    map function

---
+++ JavaScript_HiteshChai/05_loop/map.js
@@ -0,0 +1,11 @@
+const map=new Map()
+map.set('IN',"india");
+map.set('USA',"united state of america");
+map.set('fr,"France')
+map.set('NP',"Nepal");
+// console.log(map);
+for(const [key,value] of map){
+    // console.log(key,":-",value);
+}
+
+
