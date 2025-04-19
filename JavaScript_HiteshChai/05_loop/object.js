b45d36f5173a1980bf9e0291aea1517cd4cc87b6
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-04-19 13:40:24 +0530

    dom edit the element ,delete the element and use querySelector

---
+++ JavaScript_HiteshChai/05_loop/object.js
@@ -0,0 +1,10 @@
+const object={
+    js:"javascript",
+    opps:"object oriented programming",
+    py:"python",
+    cpp:"c++",
+    rb:"ruby"
+}
+for(const key in object){
+console.log(`${key } is short form is ${object[key]}`);
+}
